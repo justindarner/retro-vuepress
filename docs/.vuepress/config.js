b2da791fc2ba260df7a697ffcaf6dcbafdd4ce6f
Justin Darner <Justin.Darner@Starz.com>
2019-08-09 18:31:03 -0500

    Adds service worker configurations

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,32 @@
 module.exports = {
-  title: 'Video Game Archives',
-  description: 'Your source for retro-gaming preservation and documentation',
-  serviceWorker: {
-    updatePopup: true,
-  },
+  title: "Video Game Archives",
+  description: "Your source for retro-gaming preservation and documentation",
+  serviceWorker: true,
   themeConfig: {
+    serviceWorker: {
+      updatePopup: true
+    },
     // github
-    repo: 'justindarner/retro-vuepress',
-    repoLabel: 'Contribute!',
-    docsDir: 'docs',
-    docsBranch: 'master',
+    repo: "justindarner/retro-vuepress",
+    repoLabel: "Contribute!",
+    docsDir: "docs",
+    docsBranch: "master",
     editLinks: true,
-    editLinkText: 'Help us improve this page!',
+    editLinkText: "Help us improve this page!",
     // nav
     nav: [
       {
-        text: 'Home',
-        link: '/',
+        text: "Home",
+        link: "/"
       },
       {
-        text: 'Consoles',
-        link: '/consoles/snes/',
-      },
+        text: "Consoles",
+        link: "/consoles/snes/"
+      }
     ],
     sidebar: {
-      '/consoles/snes/': ['', '1chip/', 'csync', 'digital-audio/', 'tips'],
-      '/': [
-        ''
-      ]
+      "/consoles/snes/": ["", "1chip/", "csync", "digital-audio/", "tips"],
+      "/": [""]
     }
   }
-}
-
+};
